f40fa09073087baae3d54ec1dc14aa42647c4c6f
nikolai <n.ivanov.p@gmail.com>
2016-11-01 11:56:59 +0300

    test - 9

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -2,7 +2,7 @@
 
 cp /basic-auth.conf /etc/nginx/conf.d/default.conf
 sed -e "s/Admin/$TITLE/" -i /etc/nginx/conf.d/default.conf
-sed -e "s/app/$HOST/" -i /etc/nginx/conf.d/default.conf
+#sed -e "s/app/$HOST/" -i /etc/nginx/conf.d/default.conf
 sed -e "s/80/$PORT/" -i /etc/nginx/conf.d/default.conf
 
 htpasswd -c -b /etc/nginx/.htpasswd $USER $PASS
