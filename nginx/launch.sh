cacdcafe09a2ce35b5a20c76943c4219cdd5796c
nikolai <n.ivanov.p@gmail.com>
2016-11-02 15:07:06 +0300

    test - 3

--- nginx/launch.sh
+++ nginx/launch.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-htpasswd -c -b auth.htpasswd $USER $PASS
 
 envsubst < auth.conf > /etc/nginx/conf.d/auth.conf
 envsubst < auth.htpasswd > /etc/nginx/auth.htpasswd
 
+htpasswd -c -b /etc/nginx/auth.htpasswd $USER $PASS
+
 echo basic-auth-pwd
-cat auth.htpasswd
+cat /etc/nginx/auth.htpasswd
 
 nginx -g "daemon off;"
