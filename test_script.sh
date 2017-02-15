2af9f331ba03414948ee0bd07fc361b2ba6059e3
nikolai <n.ivanov.p@gmail.com>
2017-02-15 15:38:23 +0300

    test-3

--- test_script.sh
+++ test_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo user $NPM_USER
-npm login << EOF
-$NPM_USER
-$NPM_PASSWORD
-$NPM_EMAIL
-EOF
+#npm login << EOF
+#$NPM_USER
+#$NPM_PASSWORD
+#$NPM_EMAIL
+#EOF
 
-#echo -e "$NPM_USER\n$NPM_PASSWORD\n$NPM_EMAIL" | npm login
+echo -e "$NPM_USER\n$NPM_PASSWORD\n$NPM_EMAIL" | npm login
