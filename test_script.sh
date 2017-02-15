c2d73edacb8b4adb5634df7b5515b419a0c4f39f
nikolai <n.ivanov.p@gmail.com>
2017-02-15 15:43:47 +0300

    test-5

--- test_script.sh
+++ test_script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo user $NPM_USER
-npm adduser <<!
-$NPM_USER
-$NPM_PASSWORD
-$NPM_EMAIL
-!
-#npm login << EOF
+#npm adduser <<!
 #$NPM_USER
 #$NPM_PASSWORD
 #$NPM_EMAIL
-#EOF
+#!
+npm login << MULTILINE
+$NPM_USER
+$NPM_PASSWORD
+$NPM_EMAIL
+MULTILINE
 
 #echo -e "$NPM_USER\n$NPM_PASSWORD\n$NPM_EMAIL" | npm login
